5c78d755207ab66ceb22fd5d042c0b7706b1adbb
angeltears <1114546349@qq.com>
2017-10-18 19:40:08 +0800

    add some demo form apue

---
+++ Unix/process/tell.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "apue.h"
+
+
+static void charatatime(char *);
+
+int main(void)
+{
+    pid_t pid;
+
+    TELL_WAIT();
+
+    if (pid = fork() < 0)
+        err_sys("fork error");
+    else if (pid == 0)
+    {
+        WAIT_PARENT();
+        charatatime("output from child");
+    }
+    else
+    {
+        charatatime("output from parent");
+        TELL_CHILD(pid);
+    }
+    exit(0);
+}
+
+static void charatatime(char *str)
+{
+    char *ptr;
+    int c;
+    setbuf(stdout, NULL);
+    for(ptr =str; (c = *ptr++) != 0; )
+    {
+        putc(c, stdout);
+    }
+}
