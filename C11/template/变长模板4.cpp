80b487c7172579243bd92a1e2359db92e55f6477
angeltears <1114546349@qq.com>
2018-05-26 21:51:36 +0800

    add new

---
+++ C11/template/变长模板4.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+
+
+struct A
+{
+    A(){}
+    A(const A& a)
+    {
+        cout << "Copy Constrcuted " << __func__ << endl;
+    }
+    A(A && a)
+    {
+        cout << "Move Constructed " << __func__ << endl;
+    }
+};
+
+struct B
+{
+    B(){}
+    B(const B& b)
+    {
+        cout << "Copy Constrcuted " << __func__ << endl;
+    }
+    B(B && b)
+    {
+        cout << "Move Constructed " << __func__ << endl;
+    }
+};
+
+template <typename ... T> struct MultiTypes;
+template <typename T1, typename ... T>
+struct MultiTypes<T1, T ...> : public MultiTypes<T ...>
+{
+    T1 t1;
+    MultiTypes<T1, T ...>(T1 a, T ... b):
+         t1(a), MultiTypes<T...>(b...)
+    {
+        cout << "MultiTypes<T1, T ...>(T1 a, T ... b)" << endl;
+    }
+};
+
+
+template <> 
+struct MultiTypes<> 
+{
+    MultiTypes<>()
+    {
+        cout << "MultiTypes<>()"  << endl;
+    }
+};
+
+
+template < template <typename ...> class VariadicType, typename ... Args>
+VariadicType<Args...> Build(Args && ...args)
+{
+    return VariadicType<Args...> (std::forward<Args>(args)...);
+}
+
+
+int main()
+{
+    A a;
+    B b;
+
+    Build<MultiTypes>(a,b);
+}
