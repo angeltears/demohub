4ecb69afe87f074946f2a97eba7d4dee5944739f
angeltears <1114546349@qq.com>
2018-11-07 21:24:40 +0800

    add lz77

---
+++ 数据压缩/lz77.cc
@@ -0,0 +1,17 @@
+/**
+* Copyright: Copyright (c) 2018 angeltears-hyj, All right reserved.
+* 
+* @Functional description:lz777
+* @Author : angeltears-onter
+* @Date : 18-11-7.
+* @package : dataCompression
+*/
+
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "bit.h"
+#include "compress.h"
+g
